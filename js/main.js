585334f4baadb8b90a5dae8b205fb50cdba7620b
Molly White <molly.white5@gmail.com>
2019-04-07 20:47:00 -0400

    Ew, tabs

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
 
 function adjustRellax(illustration, rellax) {
-	const hasRellax = illustration.classList.contains('rellax');
-	if (hasRellax && window.innerWidth < 600) {
-		return removeRellax(illustration, rellax);
-	} else if (!hasRellax && window.innerWidth > 600) {
-		return addRellax(illustration, rellax);
-	}
+  const hasRellax = illustration.classList.contains('rellax');
+  if (hasRellax && window.innerWidth < 600) {
+    return removeRellax(illustration, rellax);
+  } else if (!hasRellax && window.innerWidth > 600) {
+    return addRellax(illustration, rellax);
+  }
 }
 
 function addRellax(illustration, rellax) {
-	illustration.classList.add('rellax');
-	illustration.setAttribute('data-rellax-speed', '2');
-	return new Rellax('.rellax', {
-		speed: -5
-	});
+  illustration.classList.add('rellax');
+  illustration.setAttribute('data-rellax-speed', '2');
+  return new Rellax('.rellax', {
+    speed: -5
+  });
 }
 
 function removeRellax(illustration, rellax) {
-	illustration.classList.remove('rellax');
-	illustration.removeAttribute('data-rellax-speed');
-	rellax.destroy();
-	return null;
+  illustration.classList.remove('rellax');
+  illustration.removeAttribute('data-rellax-speed');
+  rellax.destroy();
+  return null;
 }
 
 (function() {
-	document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/,'js');
+  document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/,'js');
 
-	var illustrationElement = document.getElementById('molly-and-max');
-	var rellax;
+  var illustrationElement = document.getElementById('molly-and-max');
+  var rellax;
 
-	// Add Rellax on mount if window is > 900px
-	if (window.innerWidth > 600) {
-		rellax = addRellax(illustrationElement, rellax)
-	}
+  // Add Rellax on mount if window is > 900px
+  if (window.innerWidth > 600) {
+    rellax = addRellax(illustrationElement, rellax)
+  }
 
-	// If window resizes, add or remove Rellax if needed (debounced)
-	var resizeTimeout;
-	window.addEventListener('resize', function() {
-	  clearTimeout(resizeTimeout);
-	  resizeTimeout = setTimeout(
-	  	function() {
-	  		rellax = adjustRellax(illustrationElement, rellax);
-	  	},
-	  	200
-	  );
-	});
+  // If window resizes, add or remove Rellax if needed (debounced)
+  var resizeTimeout;
+  window.addEventListener('resize', function() {
+   clearTimeout(resizeTimeout);
+   resizeTimeout = setTimeout(
+    function() {
+     rellax = adjustRellax(illustrationElement, rellax);
+   },
+   200
+   );
+ });
 })();
