c8760a6efa930d14a0ca1546dea3711904a2bb5d
Molly White <molly.white5@gmail.com>
2021-12-29 14:24:42 -0500

    a11y

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,23 @@
     'js',
   );
 
+  // Mobile nav
   var expandNavigationToggle = document.getElementById('expand-navigation-toggle');
+  expandNavigationToggle.setAttribute('aria-expanded', false);
   expandNavigationToggle.addEventListener('click', function () {
-    document.querySelector('nav.navigation').classList.toggle('expanded');
+    // Toggle and record whether the menu is expanded
+    var isExpanded = document.querySelector('nav.navigation').classList.toggle('expanded');
+
+    // Update a11y details as needed
+    this.setAttribute('aria-expanded', isExpanded);
+
+    // Switch button icon and screenreader text between hamburger menu and close button
     var faIcon = this.querySelector('i');
+    var screenreaderText = this.querySelector('span.sr-only');
     setTimeout(function () {
       faIcon.classList.toggle('fa-bars');
       faIcon.classList.toggle('fa-times');
+      screenreaderText.innerHTML = `${isExpanded ? 'Collapse' : 'Expand'} navigation`;
     }, 200);
   });
 })();
