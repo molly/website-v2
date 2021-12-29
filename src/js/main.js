755d34b56aa1ded56099987f36521a08bab127b6
Molly White <molly.white5@gmail.com>
2021-12-29 14:12:59 -0500

    Mobile-friendlier menu

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,14 @@
     /\bno-js\b/,
     'js',
   );
+
+  var expandNavigationToggle = document.getElementById('expand-navigation-toggle');
+  expandNavigationToggle.addEventListener('click', function () {
+    document.querySelector('nav.navigation').classList.toggle('expanded');
+    var faIcon = this.querySelector('i');
+    setTimeout(function () {
+      faIcon.classList.toggle('fa-bars');
+      faIcon.classList.toggle('fa-times');
+    }, 200);
+  });
 })();
