ddbe08b667105b8ce396444caecd3d654dcf36f1
Molly White <molly.white5@gmail.com>
2022-03-27 12:56:02 -0400

    If someone clicks an annotation, highlight the corresponding highlighted text

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,30 +1,62 @@
-function onHighlightClick(event) {
-  var detailsId = event.target.getAttribute('aria-details');
-  var targetComment = document.querySelector(`#${detailsId}`);
-
-  // Highlight and scroll to it
-  targetComment.classList.toggle('selected');
-  var prefersReducedMotionQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
-  var prefersReducedMotion = !prefersReducedMotionQuery || prefersReducedMotionQuery.matches;
-  targetComment.scrollIntoView({
-    behavior: prefersReducedMotion ? 'auto' : 'smooth',
-    block: 'nearest',
-  });
-
-  // Ensure this is the only highlighted comment
-  var selectedComments = document.getElementsByClassName('selected');
-  for (var i = 0; i < selectedComments.length; i++) {
-    if (selectedComments[i].id !== detailsId) {
-      selectedComments[i].classList.remove('selected');
+function deselectAllExcept(selector) {
+  var allSelected = document.getElementsByClassName('selected');
+  for (var i = 0; i < allSelected.length; i++) {
+    if (
+      allSelected[i].id !== selector &&
+      allSelected[i].getAttribute('aria-details') !== selector
+    ) {
+      allSelected[i].classList.remove('selected');
     }
   }
-
-  // Avoid bubbling through to the deselectAll function
-  event.stopPropagation();
 }
 
-function deselectAllAnnotations() {
-  var selectedComments = document.getElementsByClassName('selected');
+function makeClickHandler(isHighlight) {
+  return function onClick(event) {
+    var targetElement, selector, corresponding;
+    if (isHighlight) {
+      selector = event.target.getAttribute('aria-details');
+      targetElement = event.target;
+    } else {
+      if (event.target.classList.contains('annotation')) {
+        selector = event.target.id;
+        targetElement = event.target;
+      } else {
+        // Depending on where they click, they may have targeted a child element
+        var annotation = event.target.closest('.annotation');
+        targetElement = annotation;
+        selector = annotation.id;
+      }
+    }
+
+    if (isHighlight) {
+      corresponding = document.getElementById(selector);
+    } else {
+      corresponding = document.querySelector(`[aria-details="${selector}"]`);
+    }
+
+    // Highlight click target and corresponding element, and scroll to corresponding element
+    // If target is already highlighted, dehilight (and don't scroll)
+    const isSelected = targetElement.classList.toggle('selected');
+    corresponding.classList.toggle('selected');
+    if (isSelected) {
+      var prefersReducedMotionQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+      var prefersReducedMotion = !prefersReducedMotionQuery || prefersReducedMotionQuery.matches;
+      corresponding.scrollIntoView({
+        behavior: prefersReducedMotion ? 'auto' : 'smooth',
+        block: 'nearest',
+      });
+    }
+
+    // Ensure this is the only highlighted pair
+    deselectAllExcept(selector);
+
+    // Avoid bubbling through to the deselectAll function
+    event.stopPropagation();
+  };
+}
+
+function deselectAll() {
+  var selectedComments = document.querySelectorAll('.selected');
   for (var i = 0; i < selectedComments.length; i++) {
     selectedComments[i].classList.remove('selected');
   }
@@ -38,9 +70,14 @@ function onInitialLoad() {
 
   var highlights = document.getElementsByTagName('mark');
   for (var i = 0; i < highlights.length; i++) {
-    highlights[i].addEventListener('click', onHighlightClick);
+    highlights[i].addEventListener('click', makeClickHandler(true));
   }
-  document.addEventListener('click', deselectAllAnnotations);
+  var comments = document.getElementsByClassName('annotation');
+  for (var j = 0; j < comments.length; j++) {
+    comments[j].addEventListener('click', makeClickHandler(false));
+  }
+
+  document.addEventListener('click', deselectAll);
 }
 
 (function () {
