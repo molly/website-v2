46fd089218e47f319d0ea259b71be8b505feade7
Molly White <molly.white5@gmail.com>
2022-05-13 23:54:37 -0400

    +

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+/* global Typewriter */
 (function () {
   document.documentElement.className = document.documentElement.className.replace(
     /\bno-js\b/,
@@ -23,4 +24,42 @@
       screenreaderText.innerHTML = `${isExpanded ? 'Collapse' : 'Expand'} navigation`;
     }, 200);
   });
+
+  // Descriptions
+  var descriptions = [
+    'Wikipedia editor',
+    'aspiring gardener',
+    'friend to all dogs',
+    'cryptocurrency skeptic',
+    'possibly just a human-shaped cloud of bees',
+    'reader of <a href="/reading" target="_blank">many things</a>',
+    '<a href="/reading" target="_blank">maker of lists</a>',
+    'occasional <a href="/press" target="_blank">pundit</a>',
+    'actual-play podcast afficionado',
+    'cat whisperer',
+    'teller of bad jokes',
+    'avid indoorsman',
+    'compulsive researcher',
+    '<a href="https://www.theinformation.com/articles/cryptos-csi-how-molly-white-became-an-absolute-nightmare-for-web3-evangelists">"absolute nightmare"</a>',
+    'landlubber',
+    'seltzer sommelier',
+    'hobbyist encyclopedist',
+    'dog walk enthusiast',
+    'free knowledge advocate',
+  ];
+
+  var descriptionTarget = document.getElementById('description');
+  var welcomeTarget = document.querySelector('.welcome');
+
+  function setDescription() {
+    var choice = descriptions[Math.floor(Math.random() * descriptions.length)];
+    descriptionTarget.innerHTML = ' and<br/>' + choice;
+  }
+  setDescription();
+
+  var descriptionChanger = document.createElement('button');
+  descriptionChanger.id = 'description-changer';
+  descriptionChanger.innerHTML = 'What else?';
+  welcomeTarget.appendChild(descriptionChanger);
+  descriptionChanger.addEventListener('click', setDescription);
 })();
