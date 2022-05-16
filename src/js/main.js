e1e758d28226e598ca8e07a5cab6e2d088719bb4
Molly White <molly.white5@gmail.com>
2022-05-16 17:19:07 -0400

    Reduce dupes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,38 @@
+var DESCRIPTIONS = [
+  'Wikipedia editor',
+  'aspiring gardener',
+  'friend to all dogs',
+  'cryptocurrency skeptic',
+  'possibly just a human-shaped cloud of bees',
+  'reader of <a href="/reading" target="_blank">many things</a>',
+  '<a href="/reading" target="_blank">maker of lists</a>',
+  'occasional <a href="/press" target="_blank">pundit</a>',
+  'actual-play podcast afficionado',
+  'cat whisperer',
+  'teller of bad jokes',
+  'avid indoorsman',
+  'rabbithole explorer',
+  '<a href="https://www.theinformation.com/articles/cryptos-csi-how-molly-white-became-an-absolute-nightmare-for-web3-evangelists">"absolute nightmare"</a>',
+  'landlubber',
+  'seltzer sommelier',
+  'hobbyist encyclopedist',
+  'dog walk enthusiast',
+  'free knowledge advocate',
+  'happy camper',
+];
+
+function shuffleArray(arr) {
+  var arrayCopy = arr.slice();
+  var i, j, tmp;
+  for (i = arrayCopy.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    tmp = arrayCopy[i];
+    arrayCopy[i] = arrayCopy[j];
+    arrayCopy[j] = tmp;
+  }
+  return arrayCopy;
+}
+
 (function () {
   document.documentElement.className = document.documentElement.className.replace(
     /\bno-js\b/,
@@ -24,35 +59,16 @@
     }, 200);
   });
 
-  // Descriptions
-  var descriptions = [
-    'Wikipedia editor',
-    'aspiring gardener',
-    'friend to all dogs',
-    'cryptocurrency skeptic',
-    'possibly just a human-shaped cloud of bees',
-    'reader of <a href="/reading" target="_blank">many things</a>',
-    '<a href="/reading" target="_blank">maker of lists</a>',
-    'occasional <a href="/press" target="_blank">pundit</a>',
-    'actual-play podcast afficionado',
-    'cat whisperer',
-    'teller of bad jokes',
-    'avid indoorsman',
-    'compulsive researcher',
-    '<a href="https://www.theinformation.com/articles/cryptos-csi-how-molly-white-became-an-absolute-nightmare-for-web3-evangelists">"absolute nightmare"</a>',
-    'landlubber',
-    'seltzer sommelier',
-    'hobbyist encyclopedist',
-    'dog walk enthusiast',
-    'free knowledge advocate',
-    'happy camper',
-  ];
-
+  //Descriptions
+  var shuffledDescriptions = shuffleArray(DESCRIPTIONS);
   var descriptionTarget = document.getElementById('description');
   var welcomeTarget = document.querySelector('.welcome');
 
   function setDescription() {
-    var choice = descriptions[Math.floor(Math.random() * descriptions.length)];
+    if (shuffledDescriptions.length === 0) {
+      shuffledDescriptions = shuffleArray(DESCRIPTIONS);
+    }
+    var choice = shuffledDescriptions.pop();
     descriptionTarget.innerHTML = ' and<br/>' + choice;
   }
   setDescription();
